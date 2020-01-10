aa481135131caf8950f732182fd5a4d36994d36c
jasonluo <jasonluo@mfcgd.com>
2020-01-10 10:55:23 +0800

    first commit

---
+++ src/test/java/com/tone/amazted/AmaztedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tone.amazted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmaztedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
