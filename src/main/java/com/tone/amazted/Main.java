9dd88a4811f167218fd59d0b62737340ef2a5e20
amazted <scshfei@live.com>
2020-01-22 17:29:15 +0800

    second commit

--- src/main/java/com/tone/amazted/Main.java
+++ src/main/java/com/tone/amazted/Main.java
@@ -1,15 +1,26 @@
 package com.tone.amazted;
+import com.tone.amazted.Interface.ListService;
+import com.tone.amazted.config.ConditionConfig;
 import com.tone.amazted.config.DiConfig;
 import com.tone.amazted.config.PrePostConfig;
+import com.tone.amazted.config.TaskExcutorConfig;
+import com.tone.amazted.controller.GreetingController;
+import com.tone.amazted.controller.HelloController;
+import com.tone.amazted.service.AsyncTaskService;
 import com.tone.amazted.service.BeanWayService;
 import com.tone.amazted.service.JSR250WayService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class Main{
+@SpringBootApplication
+public class Main extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(Main.class);
+	}
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PrePostConfig.class);
-		BeanWayService beanWayService = context.getBean(BeanWayService.class);
-		JSR250WayService jsr250WayService = context.getBean(JSR250WayService.class);
-		context.close();
+		SpringApplication.run(Main.class,args);
 	}
 }
